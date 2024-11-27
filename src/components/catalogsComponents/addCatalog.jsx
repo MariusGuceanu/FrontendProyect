86e02a0419f870936873ea03a3eed687f5ded1e4
MariusGuceanu <ikcew@plaiaundi.net>
2024-11-27 11:52:57 +0100

    catalogs page and modal component

---
+++ src/components/catalogsComponents/addCatalog.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CatalogModal = () => {
+        <div></div>
+}
+
+export default CatalogModal
--- src/pages/catalogs.jsx
+++ src/pages/catalogs.jsx
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Table, Space, Button, Col } from 'antd';
+import { PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
+import CatalogModal from '../components/catalogsComponents/addCatalog';
 
-const Catalogs = () => {
+function Catalogs() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [data, setData] = useState([]);
+
+    // Main table columns
+    const columns = [
+        { title: 'Dataset ID', dataIndex: 'datasetId', key: 'datasetId', width: '12%' },
+        { title: 'Title', dataIndex: 'title', key: 'title', width: '6%' },
+        { title: 'Description', dataIndex: 'description', key: 'description', width: '17%' },
+        { title: 'Endpoints', dataIndex: 'endpoints', key: 'endpoints', width: '15%' },
+        { title: 'Offer Ids', dataIndex: 'offerIds', key: 'offerIds', width: '15%' },
+        { title: 'Keywords', dataIndex: 'keywords', key: 'keywords', width: '15%' },
+        { title: 'Format', dataIndex: 'format', key: 'format', width: '10%' },
+        {
+            title: '',
+            render: () => (
+                <Space>
+                    <Button size="small" type="primary">Edit</Button>
+                    <Button size="small" type='primary' danger>Delete</Button>
+                </Space>
+            ),
+            width: '10%',
+        },
+    ];
+
+    // Gets the stored data to show it on the table
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem('CatalogData') || '[]');
+        setData(storedData);
+    }, []);
+
+    // Adds a new row to the table with the data from the form
+    const addRowToTable = () => {
+
+        // Sets the data
+        setData((prevData) => {
+            const updatedData = [...prevData, newData];
+            localStorage.setItem('CatalogData', JSON.stringify(updatedData));
+            return updatedData;
+        });
+    };
+
+
+    // Modal logic
+    const showCatalogModal = () => setIsModalOpen(true);
+    const handleCatalogOk = () => setIsModalOpen(false);
+    const handleCatalogCancel = () => setIsModalOpen(false);
+
+    // Content display
     return (
-        <div>
-            <h1> Catalogs content</h1>
-        </div>
-    ); 
-};
+        <>
+            <Col span={24} className="button-gridP" style={{ padding: '1%' }}>
+                <Button onClick={showCatalogModal} style={{ width: '20%' }} className='action-button' size='large' type='primary'>Add Catalog</Button>
+                <CatalogModal addRowToTable={addRowToTable} isModalOpen={isModalOpen} handleCatalogOk={handleCatalogOk} handleCatalogCancel={handleCatalogCancel}></CatalogModal>
+            </Col>
+            <Table style={{ marginTop: '2%' }} className='table-contracts'
+                columns={columns}
+                scroll={{ x: '1500px', y: 93 * 6 }}
+                pagination={false} />
+        </>
+
+
+    );
+}
 
 export default Catalogs;
-
